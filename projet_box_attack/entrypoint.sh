81a599bb348239aeea6d2b17f2000a831f63ce8b
free6nk33 <free6nk33@gmail.com>
2024-11-24 21:20:19 +0100

    Update entrypoint.sh

--- projet_box_attack/entrypoint.sh
+++ projet_box_attack/entrypoint.sh
@@ -9,8 +9,5 @@ service ssh start
 # Démarrer MySQL
 service mysql start
 
-# Lancer Snort en arrière-plan pour surveiller le trafic
-snort -i eth0 -c /etc/snort/snort.conf -l /var/log/snort &
-
 # Garder le conteneur en vie
 tail -f /dev/null
